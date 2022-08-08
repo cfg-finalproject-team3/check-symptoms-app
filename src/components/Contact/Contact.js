20f8bce46ff2bc33489f1170a585ca54e0a29d10
fynefav3 <agbaruihuoma@gmail.com>
2022-08-08 15:59:34 +0100

    test contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (<div>Contact
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+  </div>);
 };
 
 export default Contact;
